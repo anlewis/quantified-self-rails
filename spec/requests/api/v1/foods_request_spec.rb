e9a2c1414f099e9489a5944ed31cd329c31ab99c
Anna Lewis <anna.noel.lewis@gmail.com>
2018-04-30 20:05:01 -0600

    Write test for update food endpoint

--- spec/requests/api/v1/foods_request_spec.rb
+++ spec/requests/api/v1/foods_request_spec.rb
@@ -24,13 +24,26 @@ describe "Foods API" do
   end
 
   it "can create a new food" do
-  food_params = { name: "Waffles", calories: 200 }
+    food_params = { name: "Waffles", calories: 200 }
 
-  post "/api/v1/foods", params: {food: food_params}
-  food = Food.last
+    post "/api/v1/foods", params: {food: food_params}
+    food = Food.last
 
-  expect(response).to be_success
-  expect(food.name).to eq(food_params[:name])
-  expect(food.calories).to eq(food_params[:calories])
-end
+    expect(response).to be_success
+    expect(food.name).to eq(food_params[:name])
+    expect(food.calories).to eq(food_params[:calories])
+  end
+
+  it "can update an existing food" do
+    id = create(:food, name: "Not a waffle").id
+    previous_name = Food.last.name
+    food_params = { name: "Waffle" }
+
+    put "/api/v1/foods/#{id}", params: {food: food_params}
+    food = Food.find_by(id: id)
+
+    expect(response).to be_success
+    expect(food.name).to_not eq(previous_name)
+    expect(food.name).to eq("Waffle")
+  end
 end
